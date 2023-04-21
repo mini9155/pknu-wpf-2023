da1ecb54c65d92f1e3eeba2dd874184df4307258
mimi9155 <hanbv@naver.com>
2023-04-21 15:42:55 +0900

    23-04-21

---
+++ Day04/Day04WpfApp/wp08_personalInfoApp/Models/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using wp08_personalInfoApp.Logics;
+
+namespace wp08_personalInfoApp.Models
+{
+    internal class Person
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private string Email
+        {
+            get => Email;
+            set
+            {
+                if (Commons.IsValidEmail(value) != true)
+                {
+                    throw new Exception("유효하지 않은 이메일 형식");
+                }
+                else
+                {
+                    Email = value;
+                }
+            }
+        }
+        private DateTime Date
+        {
+            get => Date;
+            set
+            {
+                var result = Commons.GetAge(value);
+                if (result > 120 || result <= 0)
+                {
+                    throw new Exception("유효하지 않은 생일");
+                }
+                else
+                {
+                    Date = value;
+                }
+            }
+        }
+
+        public bool IsAdult
+        {
+            get
+            {
+                return Commons.GetAge(Date) > 18;
+            }
+        }
+
+        public bool IsBirthDay
+        {
+            get
+            {
+                return DateTime.Now.Month == Date.Month && DateTime.Now.Day == Date.Day;
+            }
+        }
+
+        public string Zodiac
+        {
+            get => Commons.GetZodiac(Date);
+        }
+        public Person(string firstname, string lastname, string email, DateTime date)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Email = email;
+            Date = date;
+        }
+    }
+}
---
+++ Day04/Day04WpfApp/wp08_personalInfoApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace wp08_personalInfoApp.ViewModels
+{
+    internal class MainViewModel : ViewModelBase
+    {
+
+        // 입력 쪽 변수
+        private string inFirstName;
+        private string inLastName;
+        private string inEmail;
+        private DateTime inDate;
+
+        //결과 출력쪽 변수
+        private string outFirstName;
+        private string outLastName;
+        private string outEmail;
+        private string outDate;  // 생일 날짜 출력할 때 문자열 대체
+        private string outAdult;
+        private string outBirthDay;
+        private string outZodiac;
+
+    }
+}
